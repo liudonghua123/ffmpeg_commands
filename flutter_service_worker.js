a721fdd5f187c010b9b4d3b03d51f2346d4a7d72
liudonghua123 <liudonghua123@users.noreply.github.com>
2020-04-29 15:40:04 +0000

    deploy: 7a0b4bb5276831d5088fa17dd8339d7147c0f91c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,18 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "4934c2de71727ef1c40d7f0b23371e13",
-"/": "4934c2de71727ef1c40d7f0b23371e13",
+  "index.html": "da5153b7e439f63253e093476d73868c",
+"/": "da5153b7e439f63253e093476d73868c",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
+"assets/LICENSE": "575ea13e46c6b1bbb8b9dd68985980d9",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "12d0e258b1f2799e7a78457b72f7d312",
-"manifest.json": "73974f27b05f20b4cb13d13177ec9f50"
+"main.dart.js": "d9080ef51ea6da0697d677dc5c032133",
+"manifest.json": "23f05d4f899970ae1037aff7e14f490d"
 };
 
 self.addEventListener('activate', function (event) {
